3f9b9779652b59040a0f6ac8c48ccaf6e0c38e36
maghen2 <maghen2@gmail.com>
2024-05-27 11:10:35 +0200

    class Film

--- Exo_POO_PHP_Cinema/class/Film.php
+++ Exo_POO_PHP_Cinema/class/Film.php
@@ -1,1 +1,32 @@
 <?php
+class Film{
+    private string $titre, $synopsys;
+    private int $duree;
+    private DateTime $dateSortie;
+    private Genre $genre;
+    private Realisateur $realisateur;
+
+    // constructeur
+    public function __construct(string $titre, string $synopsys, int $duree, DateTime $dateSortie, Genre $genre, Realisateur $realisateur){
+        $this->titre = $titre;
+        $this->synopsys = $synopsys;
+        $this->duree = $duree;
+        $this->dateSortie = $dateSortie;
+        $this->genre = $genre;
+        $this->realisateur = $realisateur;
+        
+    }
+
+    //__toString
+    public function __toString(){
+        return $this->titre."(".$this->dateSortie.")";
+    }
+
+    //getInfo
+    public function getInfo(){
+
+    }
+
+    
+    
+}
