a7ef1faec1440f8cd79557678f866d737c7d15a7
maghen2 <maghen2@gmail.com>
2024-06-07 12:00:16 +0200

    Modifier les quantités de chaque produit grâce à deux points "+" et "-" positionnés de part et d'autre du nombre dans la cellule

--- Exo_POO_PHP_Appli-PHP_VG_V2.0/recap.php
+++ Exo_POO_PHP_Appli-PHP_VG_V2.0/recap.php
@@ -28,16 +28,34 @@ session_start();
         <th>Prix unitaire</th>
         <th>Quantité</th>
         <th>Prix total</th>
+        <th><a href='?id=All&action=supprimerTous'><img width= "40px" src='src/supprimer.png' alt='Supprimer tous les artcles' title='Supprimer tous les artcles' /> </a></th>
         </tr>
     </thead>
   <?php
-   if(isset($_SESSION['produit'])){
-    $total = 0;
-    foreach($_SESSION['produit'] as $index => $produit){
-      $total += $produit['Prix_total'];
-      echo "<tr> <td>".$index."</td> <td>".$produit['nom']."</td> <td>".$produit['prix']."€</thd> <td>".$produit['quantite']."</td> <td>".$produit['Prix_total']." €</td> </tr>";
+   if(isset($_SESSION['produit'])){  // on verifie si des produits existent dans la session
+
+    //Supprimer un produit en session (selon le choix de l'utilisateur)
+    // Supprimer tous les produits en session en une seule fois
+    if(isset($_GET["action"]) and $_GET["action"] !=""){ // on verifie si l'utlisateur demande une action
+      echo $_GET["action"];
+      switch($_GET["action"]){
+        case 'supprimerTous' :
+          $_SESSION['produit'] = [];
+          break;
+        case 'supprimer' :
+          if(isset($_GET["id"]) and isset($_SESSION['produit'][$_GET["id"]]) ){ // on verifie si l'utlisateur a trasmis l'id d'un produit valide
+            unset($_SESSION['produit'][$_GET["id"]]);  
+          }
+          break;  
+        default :
+      }
     }
-    echo "<tr> <th colspan=4>Total</th> <th>".$total." €</th> </tr>";
+    $total = 0;
+    foreach($_SESSION['produit'] as $id => $produit){
+      $total += $produit['Prix_total'];
+      echo "<tr> <td>".$id."</td> <td>".$produit['nom']."</td> <td>".$produit['prix']."€</thd> <td>".$produit['quantite']."</td> <td>".$produit['Prix_total']." €</td> <td><a href='?id=".$id.'&action=supprimer\'><img width="30px" src="src/supprimer.png" alt="Supprimer l\'artcle" title="Supprimer l\'artcle"> </a></td></tr>';
+    }
+    echo "<tr> <th colspan=4>Total</th> <th colspan=2>".$total." €</th> </tr>";
   }
   ?>
    
