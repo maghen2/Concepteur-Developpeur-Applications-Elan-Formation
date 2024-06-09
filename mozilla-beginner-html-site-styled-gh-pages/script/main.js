ec93fd5aff2d129b9205e07745b9bfc515cfb57f
maghen2 <maghen2@gmail.com>
2024-06-09 18:42:46 +0200

    changement dynamique du titre de la page

---
+++ mozilla-beginner-html-site-styled-gh-pages/script/main.js
@@ -0,0 +1,8 @@
+// Changement du texte de la balise h1
+let myHeading = document.querySelector("h1");
+myHeading.textContent = "Bonjour, monde !";
+
+//changement dynamique du titre de la page
+let myTitle = document.querySelector("title");
+myTitle.textContent = "Bonjour, monde !";
+
