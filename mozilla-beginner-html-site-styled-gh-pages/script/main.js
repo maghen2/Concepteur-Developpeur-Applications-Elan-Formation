e3dabd0ae852d057acafd7df41912c650402c998
maghen2 <maghen2@gmail.com>
2024-06-09 19:00:29 +0200

    Changement de l'image

--- mozilla-beginner-html-site-styled-gh-pages/script/main.js
+++ mozilla-beginner-html-site-styled-gh-pages/script/main.js
@@ -6,3 +6,9 @@ myHeading.textContent = "Bonjour, monde !";
 let myTitle = document.querySelector("title");
 myTitle.textContent = "Bonjour, monde !";
 
+// Changement de l'image
+let myPicture =  document.querySelector("img");
+myPicture.src = "https://blog.mozilla.org/opendesign/files/2019/06/FX_Design_Blog_Logos_Family.jpg";
+myPicture.setAttribute("style", "width:100%");
+
+// C
