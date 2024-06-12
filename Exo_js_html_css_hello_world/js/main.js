5826f934cfefd9b53f14477fa24fe8abb9b9bcc5
maghen2 <maghen2@gmail.com>
2024-06-12 16:30:24 +0200

    fin du cours js

--- Exo_js_html_css_hello_world/js/main.js
+++ Exo_js_html_css_hello_world/js/main.js
@@ -1,11 +1,6 @@
-//import math
-import { create, all } from 'mathjs'
-//init math
-const math = create(all,  {})
-
 // Exercice JS Hello World - Elan Formation
 console.log("Exercice JS Hello World - Elan Formation");
-
+let i = 0;
 myH1=document.querySelector("h1");
 myH1.textContent = "Hello World JS";
 
@@ -29,25 +24,46 @@ function printMultiplicationTable(){
 printMultiplicationTable();
 
 // ecrire une fonction qui invite l'utlisateur à deviner un nombre secret en au plsu 5 tentatives
-function findSecretNumber(){
-    const secretNumber = math.floor(math.random()*100);
+function findSecretNumber() {
+    const secretNumber = Math.floor(Math.random() * 100);
     const maxAttempt = 5;
-    let attemtp = maxAttempt;
+    let attempt = maxAttempt;
     let number = 0;
 
-    do{ //
-        number = window.prompt("Pourras-tu trouver le nombre secret?\n ${attemtp} tentatives restantes!");
-        attemtp -= 1;
-    } while(number != secretNumber && attemtp >= 0)
-    
-    if(number != secretNumber) {console.log("Vous avez perdu. Le nombre secret était ${secretNumber}");}
-    else {console.log("Vous avez gagné en ${maxAttempt - attemtp} tentatives!")}
+    do {
+        number = parseInt(window.prompt(`Pourras-tu trouver le nombre secret?\n ${attempt} tentatives restantes!`), 10);
+        attempt -= 1;
+        if(number === secretNumber)
+            console.log(`Vous avez gagné en ${maxAttempt - attempt} tentatives!`);    
+        else if (number > secretNumber)
+            console.log(`${number} est trop grand`);
+        else console.log(`${number} est trop petit`);
+    } while (number !== secretNumber && attempt > 0);
+    if(number == secretNumber)
+        window.alert(`Vous avez gagné en ${maxAttempt-attempt}, le nombre secret était ${secretNumber}`);
+    else 
+    window.alert(`Vous avez perdu, le nombre secret était ${secretNumber}`);
 }
 findSecretNumber();
 
 let tab = [11,77,58,67,42,18,11,19,63];
 tab.push(1);
-console.log(tab);
+console.log(`|_${tab.join('_|_')}_|`);
+
+
+//DOM acces et modiifcation des node du document
+let week = document.getElementById("week");
+week.setAttribute("style", "font-weight: bold; font-size: 15px");
+week.style.fontSize = 20;
+console.dir(week); 
+let list = document.querySelector("ul");
+let listChildrens = list.querySelectorAll("li");
+listChildrens.forEach(function(element){   
+(i%2==0)? color = "green" : color = "red";
+element.style.color = color;
+ i+=1;
+});
+
 
 
 
