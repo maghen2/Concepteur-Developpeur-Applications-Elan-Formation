e2891bf608a6022198bb72d827746b9d7e4f217f
maghen2 <maghen2@gmail.com>
2024-06-12 09:32:36 +0200

    printMultiplicationTable

--- Exo_js_html_css_hello_world/js/main.js
+++ Exo_js_html_css_hello_world/js/main.js
@@ -7,3 +7,18 @@ myH1.textContent = "Hello World JS";
 //changement dynamique du titre de la page
 let myTitle = document.querySelector("title");
 myTitle.textContent = "Bonjour, monde !";
+
+let age = 39;
+const name = "Rostant";
+
+// affiche la table de multiplication demandée
+
+function printMultiplicationTable(){
+    let nbr = window.prompt("Quel nombre dois_je afficher sa table de multiplication?",2);
+    let i = 1;
+    do{
+        console.log(nbr+" x "+i+" = "+nbr*i);
+        i++; 
+    } while(i<=10)
+}
+printMultiplicationTable();
