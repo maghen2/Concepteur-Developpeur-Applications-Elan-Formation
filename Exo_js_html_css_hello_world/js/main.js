1cb86be2771e5c94e52260f856590e01704c0ef1
maghen2 <maghen2@gmail.com>
2024-06-12 05:19:24 +0200

    m!odification du titre

--- Exo_js_html_css_hello_world/js/main.js
+++ Exo_js_html_css_hello_world/js/main.js
@@ -3,3 +3,7 @@ console.log("Exercice JS Hello World - Elan Formation");
 
 myH1=document.querySelector("h1");
 myH1.textContent = "Hello World JS";
+
+//changement dynamique du titre de la page
+let myTitle = document.querySelector("title");
+myTitle.textContent = "Bonjour, monde !";
