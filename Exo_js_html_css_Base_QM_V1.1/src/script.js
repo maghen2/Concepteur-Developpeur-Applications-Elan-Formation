d1e8646edbf7e2fc4a51e7ee83e209dc6e5aca85
maghen2 <maghen2@gmail.com>
2024-06-13 16:22:27 +0200

    ajout de l'alerte comme texte en dessous du titre Solution

--- Exo_js_html_css_Base_QM_V1.1/src/script.js
+++ Exo_js_html_css_Base_QM_V1.1/src/script.js
@@ -12,14 +12,21 @@ Police et taille du texte
 */
 
 // on recupere l'element et on lui ajoute un evenement
-let divCard = document.querySelector("div#card"); // <div id="card">
-let divCardSpan = divCard.querySelector("span"); //<div id="card"> <span>
+    const divCard = document.querySelector("div#card"); // <div id="card">
+    const divCardSpan = divCard.querySelector("span"); //<div id="card"> <span>
 
-divCard.addEventListener("click", ()=>{
+    divCard.addEventListener("click", ()=>{
     let alert = " Couleur du texte: "+window.getComputedStyle(divCardSpan).getPropertyValue('color');
     alert += "\n couleur de fond: "+window.getComputedStyle(divCard).getPropertyValue('background-color');
     alert += "\n Hauteur et largeur du carré: "+window.getComputedStyle(divCard).getPropertyValue('width')+" x "+window.getComputedStyle(divCard).getPropertyValue('height');
     alert += "\n Nom de la classe du carré: "+divCard.getAttribute("id");
     alert += "\n Police et taille du texte: "+window.getComputedStyle(divCardSpan).getPropertyValue('font-family')+" "+window.getComputedStyle(divCardSpan).getPropertyValue('font-size');
+
+    //ajout de l'alerte comme texte en dessous du titre Solution
+    const h2Solution = document.querySelector("h2#solution");
+    const pre = document.createElement("pre"); 
+    pre.innerHTML = alert;
+    h2Solution.after(pre);
+
     window.alert(alert);
 })
