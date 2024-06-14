ded358f8db5a23998711b0d471940c90eecc2f9a
maghen2 <maghen2@gmail.com>
2024-06-14 09:12:49 +0200

    nouvelle function addCard(divCard) terminéer

--- Exo_js_html_css_Base_QM_V1.1/src/Exo_js_html_css_Base_QM_V1.2.js
+++ Exo_js_html_css_Base_QM_V1.1/src/Exo_js_html_css_Base_QM_V1.2.js
@@ -12,10 +12,10 @@ function getRandomColor() {
 }
 
 // On crée la fonction copyCard(card) qui va lire la card sur laquelle l'utilisateur a copié pour la coller dans la cadr du dessous
-function copyCard(card)(){
+function copyCard(Card){
     //personnalisation des tâches avec de numéro et de couleurs differentes
     let colors = getRandomColor();
-    const newCard = taskCard.cloneNode(true); // clonage de la div .todoCard existante
+    const newCard = card.cloneNode(true); // clonage de la div .todoCard existante
     const newTextArea = newTask.querySelector(".task");
     newTextArea.setAttribute("style", "color: #"+colors[0]+"; border-color: #"+colors[0]+"; background-color: #"+colors[1]); // application de la personnalisation des styles
     const newDelBtn = newTask.querySelector(".delBtn"); // ajout de l'action de deleteBtn au nouveau child crées
@@ -23,36 +23,40 @@ function copyCard(card)(){
     newDelBtn.addEventListener("click", function(){
         deleteTask(newTask);
     });
-    counter += 1; // on incremente le compteur de tâches
-    newTextArea.value = "New task "+counter;
+
     tasksContainer.appendChild(newTask); // on ajoute la nouvelle tache au noeud parent
     divCount.innerHTML = "<p style='color: #"+colors[0]+"; font-weight: bolder;'>"+counter+" tâches</p>";
 }
 
 // On crée la fonction createCard qui créer de nouvelles card à la volet
-function addCard(card)(){
+function addCard(divCard){
     //personnalisation des tâches avec de numéro et de couleurs differentes
     let colors = getRandomColor();
-    const newCard = taskCard.cloneNode(true); // clonage de la div .todoCard existante
-    const newTextArea = newTask.querySelector(".task");
-    newTextArea.setAttribute("style", "color: #"+colors[0]+"; border-color: #"+colors[0]+"; background-color: #"+colors[1]); // application de la personnalisation des styles
-    const newDelBtn = newTask.querySelector(".delBtn"); // ajout de l'action de deleteBtn au nouveau child crées
-    newDelBtn.setAttribute("style", "color: #"+colors[0]);
-    newDelBtn.addEventListener("click", function(){
-        deleteTask(newTask);
+    // clonage de la div existante passée en paramètre
+    const newDivCard = divCard.cloneNode(true); 
+    // application de la personnalisation des styles
+    newDivCard.setAttribute("style", "color: #"+colors[0]+"; border-color: #"+colors[0]+"; background-color: #"+colors[1]); 
+    // ajout de l'action copyCard(newDivCard) à l'évenement click
+    newDivCard.addEventListener("click", function(){
+        copyCard(newDivCard);
     });
-    counter += 1; // on incremente le compteur de tâches
-    newTextArea.value = "New task "+counter;
-    tasksContainer.appendChild(newTask); // on ajoute la nouvelle tache au noeud parent
-    divCount.innerHTML = "<p style='color: #"+colors[0]+"; font-weight: bolder;'>"+counter+" tâches</p>";
+    counter += 1; // on incremente le compteur de carrée
+    newDivCard.innerHTML = "<span>Carrée "+counter+"</span>";
+    divCardContainer.appendChild(newDivCard); // on ajoute la nouvelle tache au noeud parent
+    divCount.innerHTML = "<p style='color: #"+colors[0]+"; font-weight: bolder;'>"+counter+" carrées</p>";
 }
 
-const cardContainer = document.querySelector("div#cardContainer"); // <div id="cardContainer">
-const card = cardContainer.querySelector("div.card"); // <div class="card">
+const divCardContainer = document.querySelector("div#cardContainer"); // <div id="cardContainer">
+const divCard = divCardContainer.querySelector("div.card"); // <div class="card">
+const buttonAddCard = document.querySelector("button#addCard"); // <button id="addCard">
+let counter = divCardContainer.childElementCount;
 
 // ajout de l'action copyCard à l'évenement click sur div.card
-card.addEventListener("click", function (){
+divCard.addEventListener("click", function (){
     copyCard(card);
 });
 
-
+// ajout de l'action addCard(card) au bouton 
+buttonAddCard.addEventListener("click", function(){
+    addCard(divCard);
+});
