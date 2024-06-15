f69036d3e2c60f664d0c1c94f8c235f2d4e0ba50
maghen2 <maghen2@gmail.com>
2024-06-15 13:42:49 +0200

    debugage du code

--- Exo_js_html_css_Base_QM_V1.1/src/Exo_js_html_css_Base_QM_V1.5.js
+++ Exo_js_html_css_Base_QM_V1.1/src/Exo_js_html_css_Base_QM_V1.5.js
@@ -3,10 +3,20 @@ Exercice 5 : Convertisseur de devises
 Créer une page web affichant une zone de saisie où on pourra saisir une valeur en euros afin de donner la conversion en francs La conversion (1 euro = 6.55957 francs) se fait en temps réel (pas de bouton de soumission du formulaire). Le résultat en francs est arrondi à 2 décimales. En cas de saisie non numérique un message affiche qu'on doit absolument saisir une valeur numérique.
 */
 
-const franc = document.querySelector("input#franc");
-const euro = document.querySelector("input#euro");
+let franc = document.querySelector("input#franc").value;
+let euro = document.querySelector("input#euro").value;
+console.log(franc+" francs = "+euro+" euros");
 function convertor(element){
-    if(franc.value < 0 || euro.value<0){
-        alert(element.value+"doit être un nombre positif");
+    if(franc < 0 || euro < 0){
+        // console.log(franc+" francs = "+euro+" euros");
+        window.alert(element.value+" doit être un nombre positif");
+    }
+    else{
+        if(element.name == "franc") 
+            euro = Math.floor(franc/6.55957, 2) ;
+        else
+            franc = Math.floor(euro * 6.55957, 2); 
+
+    // console.log(franc+" francs = "+euro+" euros");
     }
 }
