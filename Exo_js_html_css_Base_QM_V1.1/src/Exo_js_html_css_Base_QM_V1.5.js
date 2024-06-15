cf0f2a5f6b70dcdde12b4a98b8d9794a1a29b143
maghen2 <maghen2@gmail.com>
2024-06-15 13:25:09 +0200

    function convertor(element){}

--- Exo_js_html_css_Base_QM_V1.1/src/Exo_js_html_css_Base_QM_V1.5.js
+++ Exo_js_html_css_Base_QM_V1.1/src/Exo_js_html_css_Base_QM_V1.5.js
@@ -1,17 +1,12 @@
 /*
-Exercice 4 : Adapter la couleur d’un background en JS
-Au départ le background est gris mais lorsque l’on clique sur l’une des icônes :
-
-Le background prend sa couleur
-Le nom du réseau social apparaît
-Les bords de l’icône s’arrondissent
-Une ombre apparaît derrière l’icône
-Lorsque l’on reclique sur une icône, le background redevient gris et l’icône reprend son aspect d’origine
+Exercice 5 : Convertisseur de devises
+Créer une page web affichant une zone de saisie où on pourra saisir une valeur en euros afin de donner la conversion en francs La conversion (1 euro = 6.55957 francs) se fait en temps réel (pas de bouton de soumission du formulaire). Le résultat en francs est arrondi à 2 décimales. En cas de saisie non numérique un message affiche qu'on doit absolument saisir une valeur numérique.
 */
 
-const divSocialMedia =  document.querySelector("div#social-media"); // <div id="social-media">
-function animSocialMedia(item){
-    divSocialMedia.style.background = (window.getComputedStyle(divSocialMedia).getPropertyValue("background") == window.getComputedStyle(item).getPropertyValue("background"))? "#b4b0be" : window.getComputedStyle(item).getPropertyValue("background");
-    item.firstChild.style.display = (window.getComputedStyle(item.firstChild).getPropertyValue("display") == "none")? "block" : "none";
-    item.classList.toggle("social-media-click");
+const franc = document.querySelector("input#franc");
+const euro = document.querySelector("input#euro");
+function convertor(element){
+    if(franc.value < 0 || euro.value<0){
+        alert(element.value+"doit être un nombre positif");
+    }
 }
