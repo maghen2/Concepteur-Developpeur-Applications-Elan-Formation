a84811837f15b8fb74ce11641f5faad724415eba
maghen2 <maghen2@gmail.com>
2024-06-17 13:55:04 +0200

    construction du css code

--- responsive-bakery-website-main/assets/js/main.js
+++ responsive-bakery-website-main/assets/js/main.js
@@ -1,3 +1,19 @@
+/* EXERCICE : reproduire le travail presenté par ses propres moyens
+# Responsive Bakery Website
+## [Watch it on youtube](https://youtu.be/TTOkgUNmNk0)
+### Responsive Bakery Website
+
+- Responsive Bakery Website Design Using HTML CSS And JavaScript
+- Contains animations when scrolling.
+- Smooth scrolling in each section.
+- Contains an elegant dark theme.
+- Developed first with the Mobile First methodology, then for desktop.
+- Compatible with all mobile devices and with a beautiful and pleasant user interface.
+
+💙 Join the channel to see more videos like this. [Bedimcode](https://www.youtube.com/@Bedimcode)
+
+![preview img](/preview.png)
+*/
 /*=============== SHOW MENU ===============*/
 
 
