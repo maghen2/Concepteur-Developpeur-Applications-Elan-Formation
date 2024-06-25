24e89d204d23f009924114637bfcf07f7b6a407c
maghen2 <maghen2@gmail.com>
2024-06-25 13:53:16 +0200

    public static function seConnecter()

--- Exo_SQL_POO_PHP_Cinema/model/Connect.php
+++ Exo_SQL_POO_PHP_Cinema/model/Connect.php
@@ -4,8 +4,27 @@ namespace Model;
 // Connect.php
 //création de l'objet PDO pour la connexion à la base de données
 abstract class Connect{
-    protected $myPDO;
+    const HOST = 'localhost';
+    const DBNAME = 'cinema';
+    const CHARSET = 'utf8';
+    const USER  = 'root';
+    const PASSWORD = ''; 
 
+    public static function seConnecter(){
+        try{
+            return new \PDO(
+                'mysql:host='.self::HOST.';dbname='.self::DBNAME.';charset='.self::CHARSET,
+                self::USER,
+                self::PASSWORD
+            );
+            
+            }
+            catch(\PDOException $error){
+                return $error->getMessage();
+        } 
+    }
+    }
+/*
     public function __construct(){
         try{
             $this->myPDO = new \PDO(
@@ -20,3 +39,4 @@ abstract class Connect{
     }
 }
 
+*/
