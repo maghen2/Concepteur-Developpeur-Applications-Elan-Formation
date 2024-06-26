8e18ec367c28986b39239663075163a4d65ca3f5
maghen2 <maghen2@gmail.com>
2024-06-26 09:54:57 +0200

    <h2>Détails du film <?=$film['titre']?></h2>

--- Exo_SQL_POO_PHP_Cinema/index.php
+++ Exo_SQL_POO_PHP_Cinema/index.php
@@ -12,22 +12,24 @@ $ctrCinema = new CinemaController();
 
 $id_film = (isset($_GET['id_film']))? (int) $_GET['id_film'] : 1;
 
-if(isset($_GET["action"])){
-    switch($_GET["action"]){
-        case 'listFilms' : $ctrCinema->listFilms(); // Affichage de la liste des films
-        break;
-        case 'listActeurs' : $ctrCinema->listActeurs(); // Affichage de la liste des acteurs
-        break;
-        case 'listRealisateurs' : $ctrCinema->listRealisateurs(); // Affichage de la liste des réalisateurs
-        break;
-        case 'detailFilm' : $ctrCinema->detailFilm(); // au clic sur un film, on affiche les infos du films + casting du film (acteurs + rôles)
-        break;
-        case 'detailActeur' : $ctrCinema->detailActeur(); // au clic sur un acteur, on affiche les infos de l'acteur + filmographie (films + rôles)
-        break;
-        case 'detailRealisateur' : $ctrCinema->detailRealisateur(); // au clic sur un réalisateur, on affiche les infos du réalisateur + liste des films réalisés
-        break;
-        default :  $ctrCinema->listFilms();
-    }
+if(!isset($_GET["action"])){
+  $_GET["action"]="";
+}
+
+switch($_GET["action"]){
+  case 'listFilms' : $ctrCinema->listFilms(); // Affichage de la liste des films
+  break;
+  case 'listActeurs' : $ctrCinema->listActeurs(); // Affichage de la liste des acteurs
+  break;
+  case 'listRealisateurs' : $ctrCinema->listRealisateurs(); // Affichage de la liste des réalisateurs
+  break;
+  case 'detailFilm' : $ctrCinema->detailFilm(); // au clic sur un film, on affiche les infos du films + casting du film (acteurs + rôles)
+  break;
+  case 'detailActeur' : $ctrCinema->detailActeur(); // au clic sur un acteur, on affiche les infos de l'acteur + filmographie (films + rôles)
+  break;
+  case 'detailRealisateur' : $ctrCinema->detailRealisateur(); // au clic sur un réalisateur, on affiche les infos du réalisateur + liste des films réalisés
+  break;
+  default :  $ctrCinema->listFilms();
 }
 
 ?>
