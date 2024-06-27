88e51c80107f2152f900bb6f27ae67bfa48c249d
maghen2 <maghen2@gmail.com>
2024-06-27 16:24:18 +0200

    Dispactchage de Exo_SQL_POO_PHP_Cinema\Model\CinemaManager.php

--- Exo_SQL_POO_PHP_Cinema/controller/CinemaController.php
+++ Exo_SQL_POO_PHP_Cinema/controller/CinemaController.php
@@ -2,17 +2,20 @@
 // CinemaController.php
 namespace Controller;
 
-use Model\CinemaManager;
+use Model\PersonneManager;
+use Model\FilmManager;
 use Model\Connect;
 use \PDO;
 
 class CinemaController{
     private int $id_film, $id_acteur, $id_realisateur;
     private string $nom_genre;
-    private CinemaManager $cinemaManager;
+    private FilmManager $filmManager;
+    private PersonneManager $personneManager;
 
     public function __construct(){
-        $this->cinemaManager = new CinemaManager();
+        $this->filmManager = new FilmManager();
+        $this->personneManager = new PersonneManager();
 
         // Reception des ID envoyés par l'utilisateur
         $this->id_film = (isset($_GET['id_film']))? (int) $_GET['id_film'] : 1;
@@ -31,61 +34,61 @@ class CinemaController{
     // Lister les films
     public function listFilms(){
             // on crée un nouveau PDO pour la connexion à la base de données
-        $films = $this->cinemaManager->getFilms();
+        $films = $this->filmManager->getFilms();
         require_once("View/Film/listFilms.php");
         }
 
         // Lister les acteurs
         public function listActeurs(){
-            $acteurs = $this->cinemaManager->getActeurs();
+            $acteurs = $this->personneManager->getActeurs();
             require_once("View/Acteur/listActeurs.php");
         }
 
         
         // Lister les réalisateurs
         public function listRealisateurs(){
-            $realisateurs = $this->cinemaManager->getRealisateurs();
+            $realisateurs = $this->personneManager->getRealisateurs();
                 require_once("View/realisateur/listRealisateurs.php");
             }
 
             // Lister les genres
             public function listGenres(){
-            $genres = $this->cinemaManager->getGenres();
+            $genres = $this->filmManager->getGenres();
             require_once("View/genre/listGenres.php");
             }
             
             // Créer une vue pour ajouter un nouveau genre cinématographique dans ta base de données 
             public function addGenre(){
                 if(!empty($this->nom_genre)){
-                    if($this->cinemaManager->addGenre($this->nom_genre)) // si addGenre s'effectue correctement on redirige l'utilisateur
+                    if($this->filmManager->addGenre($this->nom_genre)) // si addGenre s'effectue correctement on redirige l'utilisateur
                     header('Location:?action=listGenres');
                 }                   
             }
 
         // au clic sur un film, on affiche les infos du films + casting du film (acteurs + rôles)    
         public function detailFilm($id_film){
-            $films = $this->cinemaManager->getFilms($id_film);
+            $films = $this->filmManager->getFilms($id_film);
             $film = $films[0];
-            $genres = $this->cinemaManager->getGenres($id_film);
-            $castings = $this->cinemaManager->getCastings(null,$id_film);
+            $genres = $this->filmManager->getGenres($id_film);
+            $castings = $this->filmManager->getCastings(null,$id_film);
             require_once("View/film/detailFilm.php");
         }
         
 
         // au clic sur un acteur, on affiche les infos de l'acteur + acteurographie (acteurs + rôles)
         public function detailActeur($id_acteur){
-            $acteurs = $this->cinemaManager->getActeurs($id_acteur);
+            $acteurs = $this->personneManager->getActeurs($id_acteur);
             $acteur = $acteurs[0];
-            $filmographies  = $this->cinemaManager->getFilmographies($id_acteur);
+            $filmographies  = $this->personneManager->getFilmographies($id_acteur);
         require_once("View/acteur/detailActeur.php");
          }
  
  
         // au clic sur un réalisateur, on affiche les infos du réalisateur + liste des films réalisés
         public function detailRealisateur($id_realisateur){
-            $realisateurs = $this->cinemaManager->getRealisateurs($id_realisateur);
+            $realisateurs = $this->personneManager->getRealisateurs($id_realisateur);
             $realisateur = $realisateurs[0];
-            $filmographies  = $this->cinemaManager->getFilmographies(null, $id_realisateur);
+            $filmographies  = $this->personneManager->getFilmographies(null, $id_realisateur);
    
    require_once("View/realisateur/detailRealisateur.php");
 
