674b8d82fb5cb040f1c4fe15237f689be9264eb7
maghen2 <maghen2@gmail.com>
2024-06-27 10:33:00 +0200

    class CinemaManager

--- Exo_SQL_POO_PHP_Cinema/model/CinemaManager.php
+++ Exo_SQL_POO_PHP_Cinema/model/CinemaManager.php
@@ -1,33 +1,30 @@
 <?php
-// CinemaController.php
-namespace Controller;
+// CinemaManager.php
+namespace Manager;
 use Model\Connect;
 use PDO;
 
-class CinemaController{
+class CinemaManager{
+        private \PDO $pdo;
 
         public function __construct(){
-
+            $this->pdo = Connect::seConnecter();
         }
 
     // Lister les films
-    public function listFilms(){
-            // on crée un nouveau PDO pour la connexion à la base de données
-        $pdo = Connect::seConnecter();
+    public function getFilms() : array{
         $sql = "SELECT film.id_film, film.titre, DATE_FORMAT(film.date_sortie_fr, '%d/%m/%Y') AS `Date`, SEC_TO_TIME(film.duree*60) AS `Duree`, film.synopsis, CONCAT(personne.prenom, ' ', personne.nom) AS `realisateur`
                 FROM film
                 JOIN realisateur on film.id_realisateur = realisateur.id_realisateur
                 JOIN personne ON personne.id_personne = realisateur.id_personne
                 ORDER BY film.date_sortie_fr DESC
         ";
-        $query = $pdo->query($sql);
-        require_once("View/Film/listFilms.php");
+        $query = $this->pdo->query($sql);
+        return $query->fetchAll(PDO::FETCH_ASSOC);
         }
 
         // Lister les acteurs
-        public function listActeurs(){
-        // on crée un nouveau PDO pour la connexion à la base de données
-        $pdo = Connect::seConnecter();
+        public function getActeurs() : array{
             $sql = "SELECT acteur.id_acteur, CONCAT(personne.prenom, ' ', personne.nom) AS acteur, personne.sexe, DATE_FORMAT(personne.date_naissance, '%d/%m/%Y') AS `date_naissance`, COUNT(casting.id_film) AS `Nombre_films`
                     FROM personne
                     JOIN acteur ON personne.id_personne = acteur.id_personne
@@ -35,15 +32,13 @@ class CinemaController{
                     GROUP by casting.id_acteur
                     ORDER by `Nombre_films` DESC
             ";
-            $query = $pdo->query($sql);
-            require_once("View/Acteur/listActeurs.php");
+            $query = $this->pdo->query($sql);
+            return $query->fetchAll(PDO::FETCH_ASSOC);
         }
 
         
         // Lister les réalisateurs
-        public function listRealisateurs(){
-            // on crée un nouveau PDO pour la connexion à la base de données
-            $pdo = Connect::seConnecter();
+        public function getRealisateurs() : array{
                 $sql = "SELECT realisateur.id_realisateur, CONCAT(personne.prenom, ' ', personne.nom) AS realisateur, 
                 personne.sexe, 
                 DATE_FORMAT(personne.date_naissance, '%d/%m/%Y') AS `date_naissance`, 
@@ -54,12 +49,12 @@ class CinemaController{
                         GROUP by film.id_realisateur
                         ORDER by `Nombre_films` DESC
                 ";
-                $query = $pdo->query($sql);
-                require_once("View/realisateur/listRealisateurs.php");
+                $query = $this->pdo->query($sql);
+                return $query->fetchAll(PDO::FETCH_ASSOC);
             }
 
             // Lister les genres
-            public function listGenres(){
+            public function getGenres(){
                       // Genres du film
                       $sql ="SELECT genre.nom_genre 
                       FROM `genre`
@@ -67,7 +62,7 @@ class CinemaController{
                       ";                
             $query = $this->pdo->query($sql);
             $this->data['film_genres'] = $query->fetchAll(PDO::FETCH_ASSOC);
-            require_once("View/genre/listGenres.php");
+            return $query->fetchAll(PDO::FETCH_ASSOC);
             }
             
             // Créer une vue pour ajouter un nouveau genre cinématographique dans ta base de données 
@@ -83,8 +78,7 @@ class CinemaController{
                     $query->execute(["nom_genre" => $nom_genre]);
                 }    
                 
-                header('Location:?action=listGenres');
-                // require_once("View/genre/addGenre.php");
+                return $query->fetchAll(PDO::FETCH_ASSOC);
             }
 
         // au clic sur un film, on affiche les infos du films + casting du film (acteurs + rôles)    
@@ -138,7 +132,7 @@ class CinemaController{
             $query->execute(["id_film" => $id_film]);
             $this->data["casting"] = $query->fetchAll(PDO::FETCH_ASSOC);
             
-            require_once("View/film/detailFilm.php");
+            return $query->fetchAll(PDO::FETCH_ASSOC);
 
         }
         
@@ -174,7 +168,7 @@ class CinemaController{
  
     $query = $this->pdo->prepare($sql);
     $query->execute(["id_acteur" => $id_acteur]);
-    $this->data["acteur"] = $query->fetch(PDO::FETCH_ASSOC);
+    return $query->fetchAll(PDO::FETCH_ASSOC);
     
     // filmographie de l'acteur(rôles / films)   
     $sql = "SELECT
@@ -202,14 +196,14 @@ class CinemaController{
    ";
     $query = $this->pdo->prepare($sql);
     $query->execute(["id_acteur" => $id_acteur]);
-    $this->data["filmographie"] = $query->fetchAll(PDO::FETCH_ASSOC);
+    return $query->fetchAll(PDO::FETCH_ASSOC);
     
     require_once("View/acteur/detailActeur.php");
  
          }
  
  
-        // au clic sur un réalisateur, on affiche les infos du réalisateur + liste des films réalisés
+        // au clic sur un réalisateur, on affiche les infos du réalisateur + gete des films réalisés
         public function detailRealisateur($id_realisateur){
                        // infos de l'realisateur
                        $sql = "SELECT CONCAT(personne.prenom, ' ', personne.nom) AS realisateur, 
@@ -226,7 +220,7 @@ class CinemaController{
 
    $query = $this->pdo->prepare($sql);
    $query->execute(["id_realisateur" => $id_realisateur]);
-   $this->data["realisateur"] = $query->fetch(PDO::FETCH_ASSOC);
+   return $query->fetchAll(PDO::FETCH_ASSOC);
    
    // filmographie du realisateur(rôles / films)   
    $sql = "SELECT
@@ -245,46 +239,9 @@ class CinemaController{
   ";
    $query = $this->pdo->prepare($sql);
    $query->execute(["id_realisateur" => $id_realisateur]);
-   $this->data["filmographie"] = $query->fetchAll(PDO::FETCH_ASSOC);
-   
-   require_once("View/realisateur/detailRealisateur.php");
+   return $query->fetchAll(PDO::FETCH_ASSOC);
 
         }
 
         }
 
-
-/*
-    function listFilms(){
-        $pdo = Connect::seConnecter();
-        $sql = "SELECT * FROM film";
-        $query = $pdo->prepare($sql);
-        $query->execute();
-        $films = $query>fetchAll();
-        foreach($films as $film){
-            echo $film['titre']."<br>";
-        }
-
-
-class CinemaController{
-    // Lister les films
-    private $model;
-    private $view;
-
-
-    public function __construct(){
-        $this->model = new CinemaModel();
-        $this->view = new CinemaView();
-    }
-
-    public function listFilms(){
-        $films = $this->model->getFilms();
-        $this->view->listFilms($films);
-    }
-
-    public function listActeurs(){
-        $acteurs = $this->model->getActeurs();
-        $this->view->listActeurs($acteurs);
-    }
-}
-*/
